b2eb470788e5ed7421396eb75a60a7d6bcd6ef9d
Angga Lingga <aslingga@gmail.com>
2022-01-12 17:24:28 +0700

    Add team information by using modal

--- js/custom.js
+++ js/custom.js
@@ -16,3 +16,20 @@ $("#mc-form-submit").click(function() {
 
     return false; // avoid to execute the actual submit of the form.
 });
+
+$(".team-image-link").click(function() {	
+	profileCard = $(this).closest(".profile-card");
+	profileImage = $(profileCard).find("img");
+	profileName = $(profileCard).find("h5");
+	profileRole = $(profileCard).find(".role");
+	profileBio = $(profileCard).find(".bio");
+	profileLinkedIn = $(profileCard).find(".linked-in");
+	
+	$('#team-modal-image').attr("src", $(profileImage).attr("src"));
+	$('#team-modal-name').text($(profileName).text());
+	$('#team-modal-role').text($(profileRole).text());
+	$('#team-modal-bio').text($(profileBio).text());
+	$('#team-modal-linked-in').text($(profileLinkedIn).text());
+	
+	return false;
+});
